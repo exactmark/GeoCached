23225ebf7e6967e781411535288ca57d9cc068ea
Mark Fowler <exactmark@gmail.com>
2021-03-18 16:02:02 -0400

    Added actual doc

--- Server/Dev/requests_actual.py
+++ Server/Dev/requests_actual.py
@@ -2,6 +2,7 @@ import requests
 import json
 
 base_url = 'http://127.0.0.1:5000/'
+# base_url = 'https://exactmark.pythonanywhere.com/'
 
 user_list = [{'id': 'Mark', 'pw': 'WeakPw'},
              {'id': 'Vishal', 'pw': 'WeakPw'},
