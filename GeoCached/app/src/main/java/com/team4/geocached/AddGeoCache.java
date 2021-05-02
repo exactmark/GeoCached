6989cb6309d923e5f7d009e25d0d6c80da4e8235
Vishal Bilagi <vishalb98@gmail.com>
2021-05-02 17:45:03 -0400

    remove background placeholder

--- GeoCached/app/src/main/java/com/team4/geocached/AddGeoCache.java
+++ GeoCached/app/src/main/java/com/team4/geocached/AddGeoCache.java
@@ -237,7 +237,7 @@ public class AddGeoCache extends AppCompatActivity {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
+            imageView.setBackgroundResource(0);
             imageView.setImageBitmap(image);
         }
 
